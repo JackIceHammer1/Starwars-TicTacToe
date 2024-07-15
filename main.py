eedf3c351b282ef0e4ed682716f013521a7bf40f
Jack <jackthor0608@gmail.com>
2024-07-15 12:40:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ LINE_COLOR = (23, 145, 135)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Star Wars Tic-Tac-Toe')
 
+# Board
+board = [[None for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
+
 # Draw Lines
 def draw_lines():
     screen.fill(BG_COLOR)
@@ -27,12 +30,38 @@ def draw_lines():
     pygame.draw.line(screen, LINE_COLOR, (SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), 15)
     pygame.draw.line(screen, LINE_COLOR, (2 * SQUARE_SIZE, 0), (2 * SQUARE_SIZE, HEIGHT), 15)
 
+# Draw Figures
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 'X':
+                pygame.draw.line(screen, (84, 84, 84), (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + 50),
+                                 (col * SQUARE_SIZE + SQUARE_SIZE - 50, row * SQUARE_SIZE + SQUARE_SIZE - 50), 15)
+                pygame.draw.line(screen, (84, 84, 84), (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + SQUARE_SIZE - 50),
+                                 (col * SQUARE_SIZE + SQUARE_SIZE - 50, row * SQUARE_SIZE + 50), 15)
+            elif board[row][col] == 'O':
+                pygame.draw.circle(screen, (239, 231, 200), 
+                                   (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 60, 15)
+
 # Main Loop
+player = 'X'
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX = event.pos[0]  # x
+            mouseY = event.pos[1]  # y
+
+            clicked_row = mouseY // SQUARE_SIZE
+            clicked_col = mouseX // SQUARE_SIZE
+
+            if board[clicked_row][clicked_col] is None:
+                board[clicked_row][clicked_col] = player
+                player = 'O' if player == 'X' else 'X'
 
     draw_lines()
+    draw_figures()
     pygame.display.update()
