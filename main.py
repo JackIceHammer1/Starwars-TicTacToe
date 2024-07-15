e2926b2a5cf7a5e66efd7ddc6018456daf5ab3b8
Jack <jackthor0608@gmail.com>
2024-07-15 15:10:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ SQUARE_SIZE = WIDTH // BOARD_COLS
 # Colors
 BG_COLOR = (28, 170, 156)
 LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (84, 84, 84)
 
 # Initialize Screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -35,12 +37,12 @@ def draw_figures():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] == 'X':
-                pygame.draw.line(screen, (84, 84, 84), (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + 50),
+                pygame.draw.line(screen, CROSS_COLOR, (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + 50),
                                  (col * SQUARE_SIZE + SQUARE_SIZE - 50, row * SQUARE_SIZE + SQUARE_SIZE - 50), 15)
-                pygame.draw.line(screen, (84, 84, 84), (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + SQUARE_SIZE - 50),
+                pygame.draw.line(screen, CROSS_COLOR, (col * SQUARE_SIZE + 50, row * SQUARE_SIZE + SQUARE_SIZE - 50),
                                  (col * SQUARE_SIZE + SQUARE_SIZE - 50, row * SQUARE_SIZE + 50), 15)
             elif board[row][col] == 'O':
-                pygame.draw.circle(screen, (239, 231, 200), 
+                pygame.draw.circle(screen, CIRCLE_COLOR, 
                                    (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 60, 15)
 
 # Check Win
@@ -48,22 +50,44 @@ def check_win(player):
     # Vertical Win Check
     for col in range(BOARD_COLS):
         if board[0][col] == board[1][col] == board[2][col] == player:
+            draw_vertical_winning_line(col, player)
             return True
 
     # Horizontal Win Check
     for row in range(BOARD_ROWS):
         if board[row][0] == board[row][1] == board[row][2] == player:
+            draw_horizontal_winning_line(row, player)
             return True
 
     # Ascending Diagonal Win Check
     if board[2][0] == board[1][1] == board[0][2] == player:
+        draw_asc_diagonal(player)
         return True
 
     # Descending Diagonal Win Check
     if board[0][0] == board[1][1] == board[2][2] == player:
+        draw_desc_diagonal(player)
         return True
 
     return False
+
+def draw_vertical_winning_line(col, player):
+    posX = col * SQUARE_SIZE + SQUARE_SIZE // 2
+    color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
+    pygame.draw.line(screen, color, (posX, 15), (posX, HEIGHT - 15), 15)
+
+def draw_horizontal_winning_line(row, player):
+    posY = row * SQUARE_SIZE + SQUARE_SIZE // 2
+    color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
+    pygame.draw.line(screen, color, (15, posY), (WIDTH - 15, posY), 15)
+
+def draw_asc_diagonal(player):
+    color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
+    pygame.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), 15)
+
+def draw_desc_diagonal(player):
+    color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
+    pygame.draw.line(screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), 15)
 
 # Main Loop
 player = 'X'
